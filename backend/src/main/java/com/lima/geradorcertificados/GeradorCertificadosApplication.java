fc359193f28aa918356c00ddc6a99102fc6207f3
Amanda Oliveira de Lima <amanda@localhost.localdomain>
2020-07-05 16:44:24 -0300

    Create backend project

---
+++ backend/src/main/java/com/lima/geradorcertificados/GeradorCertificadosApplication.java
@@ -0,0 +1,13 @@
+package com.lima.geradorcertificados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorCertificadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorCertificadosApplication.class, args);
+	}
+
+}
